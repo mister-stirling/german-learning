943e2df2c03eff3c4175bee948f893d6e0bbe8c8
mister-stirling <mister.stirlingen@gmail.com>
2025-08-21 08:29:12 +0200

    Add files via upload

--- Sources_pour_les_nerds/stats_finales.R
+++ Sources_pour_les_nerds/stats_finales.R
@@ -25,6 +25,10 @@ dates_raw <- as.character(data[[6]])
 dates_parsed <- dmy(dates_raw)
 print(dates_parsed)
 
+#Récupération du statut du batch (outlier = "yes")
+#Les outliers sont les batchs de pages où les moyennes de temps sont sous-estimées à cause de pages <180 mots
+data$outlier_status<- as.character(data[[9]])
+
 # Calculs dérivés
 data$words_per_minute <- with(data, total_words / time_minutes)
 data$time_per_word <- with(data, time_minutes / total_words)
@@ -32,6 +36,16 @@ data$new_words_on_total <- with(data, 100 * new_words / total_words)
 data$consult_words_on_total <- with(data, 100 * consult_words / total_words)
 total_dur_hour <- (sum(data$time_minutes, na.rm = TRUE))/60
 
+# --- OUTLIERS: masque et sous-ensemble propre
+# Créer dataset sans batchs outliers :
+mask_keep <- is.na(data$outlier_status) | tolower(trimws(data$outlier_status)) != "yes"
+data_no_outliers <- data[mask_keep, ]
+
+#Moyenne des 20 derniers points NON-OUTLIERS :
+valid_values_clean <- data_no_outliers$words_per_minute[
+  !is.na(data_no_outliers$words_per_minute) & data_no_outliers$words_per_minute != 0
+]
+mean_last20_clean <- mean(tail(valid_values_clean, 20))
 
 # Fonction pour tracer et afficher pente
 plot_with_slope <- function(x, y, xlab, ylab, main, data_name = "", y_tick_step = 1) {
@@ -65,30 +79,53 @@ plot_with_slope <- function(x, y, xlab, ylab, main, data_name = "", y_tick_step 
 
 # Tracer et calculer les pentes pour chaque graphique
 
+# Filtrer les valeurs valides (ni NA ni 0)
+valid_values <- data$words_per_minute[!is.na(data$words_per_minute) & data$words_per_minute != 0]
+
+# Moyenne des 20 derniers points valides
+mean_last20 <- mean(tail(valid_values, 20))
+
+# Figure 1 avec régression + ligne horizontale de la moyenne
 png("graphes_export/fig1_vitesse_etude.png", width = 800, height = 600)
 plot_with_slope(
   data$page_number,
   data$words_per_minute,
   xlab = "Numéro de page",
   ylab = "Mots par minute",
-  main = "Figure 1. Progression de la vitesse d'étude",
-  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)",
+  main = "Figure 1. Progression de la vitesse d'étude (avec batchs w/ outliers)",
+  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)",
   y_tick_step = 3
 )
+
+# Ligne horizontale = moyenne des 20 derniers points
+abline(h = mean_last20, col = "darkgreen", lwd = 2, lty = 2)
+
+# Légende en bas à droite
+legend("bottomright", 
+       legend = paste0("Moyenne derniers 20 points = ", round(mean_last20, 1), " mpm"),
+       col = "darkgreen", lwd = 2, lty = 2, cex = 0.9, bty = "n")
+
 dev.off()
 
-#MPM
-#png("graphes_export/fig2_temps_par_mot.png", width = 800, height = 600)
-#plot_with_slope(
-# data$page_number,
-# data$time_per_word,
-# xlab = "Numéro de page",
-# ylab = "Minutes par mot",
-# main = "Figure 2. Vitesse d'étude (temps par mot)",
- # data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)",
-# y_tick_step = 0.1
-#)
-#dev.off()
+# --- Figure 1 SANS outliers
+png("graphes_export/fig1_vitesse_etude_SANS_outliers.png", width = 800, height = 600)
+plot_with_slope(
+  data_no_outliers$page_number,
+  data_no_outliers$words_per_minute,
+  xlab = "Numéro de page",
+  ylab = "Mots par minute",
+  main = "Figure 1. Progression de la vitesse d'étude (sans batchs w/ outliers)",
+  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)",
+  y_tick_step = 3
+)
+
+abline(h = mean_last20_clean, col = "darkgreen", lwd = 2, lty = 2)
+legend(
+  "bottomright",
+  legend = paste0("Moyenne derniers 20 points = ", round(mean_last20_clean, 1), " mpm"),
+  col = "darkgreen", lwd = 2, lty = 2, cex = 0.9, bty = "n"
+)
+dev.off()
 
 #New words per page
 png("graphes_export/fig2_mots_nouveaux.png", width = 800, height = 600)
@@ -97,40 +134,44 @@ plot_with_slope(
   data$new_words_on_total,
   xlab = "Numéro de page",
   ylab = "Mots nouveaux (%)",
-  main = "Figure 2. Taux de mots découverts par page",
-  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)",
+  main = "Figure 2. Taux de mots découverts par page (sans outliers)",
+  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)",
   y_tick_step = 4
 )
 dev.off()
 
 #Superposition
 
+#data_no_outliers$page_number,
+#data_no_outliers$words_per_minute,
+#data_no_outliers
+
 plot_superpose_fig1_fig2 <- function(data) {
-  valid1 <- !is.na(data$page_number) & !is.na(data$words_per_minute)
-  valid3 <- !is.na(data$page_number) & !is.na(data$new_words_on_total)
+  valid1 <- !is.na(data_no_outliers$page_number) & !is.na(data_no_outliers$words_per_minute)
+  valid3 <- !is.na(data_no_outliers$page_number) & !is.na(data_no_outliers$new_words_on_total)
   
   # Tracer figure 1 : mots par minute (axe Y gauche)
-  plot(data$page_number[valid1], data$words_per_minute[valid1],
+  plot(data_no_outliers$page_number[valid1], data_no_outliers$words_per_minute[valid1],
        type = "l", pch = 3, col = "blue",
        xlab = "Numéro de page", ylab = "Mots par minute",
-       main = "Figure 3. Figure 1 & 2 superposées",
+       main = "Figure 3. Figure 1 & 2 superposées (sans batchs w/ outliers)",
        cex.main = 0.8, cex.lab = 0.7, cex.axis = 0.7, cex = 0.4)
-  mtext("*Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)", side = 1, line = 4, cex = 0.6, col = "black")
+  mtext("*Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)", side = 1, line = 4, cex = 0.6, col = "black")
   # Calcul et affichage pente pour figure 1
-  model1 <- lm(words_per_minute ~ page_number, data = data[valid1, ])
+  model1 <- lm(words_per_minute ~ page_number, data = data_no_outliers[valid1, ])
   #abline(model1, col = "blue", lwd = 2)
   legend_text1 <- paste0("Vitesse étude mots/min (pente = ", round(coef(model1)[2], 3), ")")
   
   # Ajouter figure 3 : mots nouveaux en % (axe Y droit)
   par(new = TRUE)
-  plot(data$page_number[valid3], data$new_words_on_total[valid3],
+  plot(data_no_outliers$page_number[valid3], data_no_outliers$new_words_on_total[valid3],
        type = "l", pch = 17, col = "red",
        axes = FALSE, xlab = "", ylab = "", cex = 0.4)
   axis(side = 4, col = "red", col.axis = "red", cex.axis = 0.7)
   mtext("Mots nouveaux (%)", side = 4, line = 2, col = "red", cex = 0.7)
   
   # Calcul et affichage pente pour figure 3
-  model3 <- lm(new_words_on_total ~ page_number, data = data[valid3, ])
+  model3 <- lm(new_words_on_total ~ page_number, data = data_no_outliers[valid3, ])
   #abline(model3, col = "red", lwd = 2)
   legend_text3 <- paste0("Mots nouveaux/total % (pente = ", round(coef(model3)[2], 3), ")")
   
@@ -158,7 +199,7 @@ plot_with_slope(
   xlab = "Numéro de page",
   ylab = "Mots consultés (%)",
   main = "Figure 5. Taux de mots consultés (usage dico) par page",
-  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)",
+  data_name = "Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)",
   y_tick_step = 2
 )
 dev.off()
@@ -166,10 +207,10 @@ dev.off()
 # Histo pour temps total
 p <- ggplot(data, aes(x = page_number, y = time_minutes)) +
   geom_col(fill = "steelblue") +
-  scale_x_continuous(breaks = seq(10, 200, by = 20)) +
+  scale_x_continuous(breaks = seq(10, 380, by = 20)) +
   labs(x = "Numéro de page", y = "Durée (min)", 
        title = "Figure 5. Temps total d'étude effectif",
-       caption = "*Livre: Außenseiter 1 - Fauxhumain1 (p9 - p200)") +
+       caption = "*Livre: Außenseiter 1 - Fauxhumain1 (p9 - p378)") +
   theme_minimal() +
   theme(
     plot.title = element_text(
@@ -198,3 +239,22 @@ ggsave(
   width = 6,
   height = 4
 )
+
+# Vitesse sur les 50 dernières pages :
+valid_values <- data$words_per_minute[!is.na(data$words_per_minute) & data$words_per_minute != 0]
+mean_last50 <- mean(tail(valid_values, 50))
+print(mean_last50)
+
+# --- Moyenne des 20 derniers points valides SANS outliers
+print("Last 20 words per minute, no outliers:")
+print(mean_last20_clean)
+print("End no outliers")
+
+
+#Vitesse intervalles 
+
+res <- with(subset(data, !is.na(page_number) & !is.na(words_per_minute) & words_per_minute > 0),
+            tapply(words_per_minute,
+                   cut(page_number, breaks = c(0, 50, 100, 200, 300, 380), right = FALSE),
+                   mean))
+print(res)
